729d7b58eb8a5c316ccee099e04626c0dc7b3379
Shyam vangapandu <ganesh050408@gmail.com>
2026-02-28 20:21:58 +0530

    Add files via upload

---
+++ python3.py
@@ -0,0 +1,9 @@
+print("relation between two numbers")
+a=int(input("enter the first number"))
+b=int(input("enter the second number"))
+if a>b:
+    print("a is greater than b")
+elif a<b:
+    print("a is less than b")
+else:
+    print("a is equal to b")
